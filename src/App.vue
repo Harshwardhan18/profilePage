e7697d3bb4a7c5dc63df527a1c03ddd3c87e617e
Harshwardhan <harshw.pauranik@gmail.com>
2019-12-12 13:02:54 +0530

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import header from './components/pageHeader/header.vue'
+import footer from './components/pageFooter/footer.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    appHeader : header
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
